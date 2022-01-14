d7ec477d27864cedfddad55e2bf373ddc3031fa7
humayed2835 <humayedhosenspc2@gmail.com>
2022-01-15 00:28:33 +0600

    file created

---
+++ PhpMyAdmin/iawd-2104.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.1
+ */
+
+/**
+ * Database `iawd-2104`
+ */
+
+/* `iawd-2104`.`banners` */
+$banners = array(
+  array('id' => '1','sub_title' => 'we are ready to help you','title_top' => 'Financial Humayed','title_bottom' => '& Consulting','detail' => 'This finance HTML template is 100% free of charge provided by TemplateMo for everyone. This is a multiple-page version with different HTML pages. ','image_location' => 'uploads/banner/1.jpg','active_status' => '1'),
+  array('id' => '3','sub_title' => 'we are here to support you','title_top' => 'Humayed Hosen','title_bottom' => '& Management','detail' => 'This finance HTML template is 100% free of charge provided by TemplateMo for everyone. This is a multiple-page version with different HTML pages. dafd','image_location' => 'uploads/banner/3.jpg','active_status' => '1'),
+  array('id' => '4','sub_title' => 'we are ready to help you','title_top' => 'Financial Analysisasa','title_bottom' => '& Management','detail' => 'This finance HTML template is 100% free of charge provided by TemplateMo for everyone. This is a multiple-page version with different HTML pages. ','image_location' => 'uploads/banner/4.jpg','active_status' => '1')
+);
+
+/* `iawd-2104`.`guest_messages` */
+$guest_messages = array(
+  array('id' => '2','guest_name' => 'Humayed Hosen','guest_email' => 'humayedhosenspc2@gmail.com','guest_subject' => 'Javascript','guest_meassage' => 'Request a call back
+ETIAM SUSCIPIT ANTE A ODIO CONSEQUAT','read_status' => '2')
+);
+
+/* `iawd-2104`.`users` */
+$users = array(
+  array('id' => '1','user_name' => 'mahedi hana','user_email' => 'shaon@mail.com','user_mobile' => '01874892835','user_pass' => '22e8cca6743ac6835df5fd742f0afef6'),
+  array('id' => '2','user_name' => 'mahedi hana','user_email' => 'shaon@mail.com','user_mobile' => '01874892835','user_pass' => '36924c298f2176c55166976d2b8807b7'),
+  array('id' => '3','user_name' => 'Santo','user_email' => 'humayedhosenspc2@gmail.com','user_mobile' => '01874892835','user_pass' => '36924c298f2176c55166976d2b8807b7'),
+  array('id' => '4','user_name' => 'humayed Hosen','user_email' => 'humayedhosenspc@gmail.com','user_mobile' => '01874892835','user_pass' => '36924c298f2176c55166976d2b8807b7')
+);
